ae7cf6de3a2798fe7fefa94e862b1c7cf9786d99
PatriciaJurjut <p.jurjut@gmail.com>
2022-05-14 22:32:08 +0300

    Refactor constants

--- water_refill_monitoring.py
+++ water_refill_monitoring.py
@@ -2,8 +2,11 @@ from time import sleep
 import smbus
 from datetime import datetime
 from utils import current_time_milliseconds
-from constants import WATER_I2C_ADDRESS, WATER_SENSOR_ANALOG_PIN_INPUT
-from constants import WATER_SERVO_NUMBER, WATER_SERVO_ANGLE_OPEN, WATER_SERVO_ANGLE_CLOSED
+from constants import WATER_I2C_ADDRESS as I2C_address
+from constants import WATER_SENSOR_ANALOG_PIN_INPUT as analog_pin_input
+from constants import WATER_SERVO_NUMBER as servo_number
+from constants import WATER_SERVO_ANGLE_OPEN as open_angle
+from constants import WATER_SERVO_ANGLE_CLOSED as closed_angle
 from constants import WATER_LEVEL_LOWER_THRESHOLD as lower_threshold
 from constants import WATER_LEVEL_UPPER_THRESHOLD as upper_threshold
 from servo import set_servo_angle
@@ -22,19 +25,19 @@ def get_last_watering_date():
     return ts
 
 def watering_service():
-        current_water_level = get_current_water_level(WATER_I2C_ADDRESS, WATER_SENSOR_ANALOG_PIN_INPUT)
+        current_water_level = get_current_water_level(I2C_address, analog_pin_input)
         if(current_water_level <= lower_threshold):
             refill_water_bowl()
         sleep(3)
                 
 def refill_water_bowl():
-    current_water_level = get_current_water_level(WATER_I2C_ADDRESS, WATER_SENSOR_ANALOG_PIN_INPUT)
+    current_water_level = get_current_water_level(I2C_address, analog_pin_input)
     if current_water_level <= lower_threshold:
         while not is_refill_finished(upper_threshold):
-            set_servo_angle(WATER_SERVO_ANGLE_OPEN, WATER_SERVO_NUMBER)
+            set_servo_angle(open_angle, servo_number)
     #refill is done, set angle in initial position
-    set_servo_angle(WATER_SERVO_ANGLE_CLOSED, WATER_SERVO_NUMBER)
+    set_servo_angle(closed_angle, servo_number)
     
 def is_refill_finished(fullness_percentage):
-    return get_current_water_level(WATER_I2C_ADDRESS, WATER_SENSOR_ANALOG_PIN_INPUT) >= fullness_percentage
+    return get_current_water_level(I2C_address, analog_pin_input) >= fullness_percentage
     
