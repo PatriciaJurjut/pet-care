cd7d31af8dfc7439981cdba7c442aa259fa5e818
PatriciaJurjut <p.jurjut@gmail.com>
2022-06-13 16:27:31 +0300

    Refactor everything

--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -1,7 +1,9 @@
 import time
+
+
 def current_time_milliseconds():
     return round(time.time() * 1000)
 
+
 def datetime_to_timestamp(datetime_variable):
     return int(round(datetime_variable.timestamp()))
-
